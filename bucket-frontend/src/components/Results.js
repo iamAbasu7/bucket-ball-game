e695af3ed5dde0a6d7b74948a23bb0b0c414779c
Antar Basu <antar.basu@zeeve.io>
2024-04-15 10:52:41 +0530

    change in display result

--- bucket-frontend/src/components/Results.js
+++ bucket-frontend/src/components/Results.js
@@ -3,12 +3,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Results() {
-  const [bucketNames, setBucketNames] = useState([]);
-  const [output, setOutput] = useState([]);
+  const [output, setOutput] = useState('');
 
-  function reformatString(input) {
-    const formattedString = input.replace(/Bucket (?=[A-Z])/g, 'Bucket\n');
-    return formattedString;
+function reformatString(input) {
+  // Join the array of strings into a single string with line breaks
+  const joinedString = input.join('\n');
+
+  // Replace occurrences of 'Bucket' followed by a capital letter with 'Bucket' followed by a line break
+  const formattedString = joinedString.replace(/Bucket (?=[A-Z])/g, 'Bucket\n');
+
+  return formattedString;
 }
 
   useEffect(() => {
@@ -16,8 +20,6 @@ function Results() {
       try {
         const response = await axios.get('http://localhost:9001/api/v1/buckets');
         setOutput(reformatString(response.data));
-        const a = reformatString(output);
-        console.log(a)
       } catch (error) {
         console.error('Error fetching bucket names:', error);
       }
@@ -29,12 +31,7 @@ function Results() {
     <div className='mx-5'>
       <h2 className='p-2'>Bucket Results</h2>
       <div>
-      {output}
-        {/* {bucketNames.map((bucketName, index) => (
-          <div key={index}>
-            <p>{bucketName}</p>
-          </div>
-        ))} */}
+        {output}
       </div>
     </div>
   );
